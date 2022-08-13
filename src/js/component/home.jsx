71459c211d1542826f2eda465228522f422901c1
Bairon00 <bairon97@icloud.com>
2022-08-13 03:30:24 +0000

    ta dificil

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	/*const[aa,setAa]=useState([])
+	const[tareas,setTareas]=useState("");
+	const receptor=(e)=>{e.preventDefault()};
+	
+	
+
+
+	return(
+		<div className="text-center">
+			<form type="sumbit" onSubmit={receptor} >
+			<h1>LISTA</h1>
+			<input type="text" />
+			</form>
+		</div>
+	);*/
+	const[listadetareas,seListadetareas]=useState([])
+	const tareas =(e)=>{
+	e.preventDefault();
+	const conjuntodetareas=setAa([...listadetareas,e.target[0].value]);
+	console.log(conjuntodetareas[0])
+	
+	
+};
+
+
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<form type="submit"  onSubmit={tareas}>
+			<h1 className="text-center mt-5">Tareas</h1>
+			<input type="text"  />
+			
+			<ul>
+
+			</ul>
+			</form>
 		</div>
 	);
 };
