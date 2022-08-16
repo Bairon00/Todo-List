4c1f641d9bbf9b41256a2ffde7df94d3bdd78853
Bairon00 <bairon97@icloud.com>
2022-08-16 00:25:39 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,7 +5,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	
+	const [btex,setbtex]=useState("")
 	const[listadetareas,setListadetareas]=useState([])
 	const tareas =(e)=>{e.preventDefault();
 	setListadetareas([...listadetareas,e.target[0].value])
@@ -16,15 +16,19 @@ const Home = () => {
 }
 
 
+	
+	
+
+
 
 	return (
 		<div className="text-center">
 			<form type="submit"  onSubmit={tareas}>
 			<h1 className="text-center mt-5">Tareas</h1>
-			<input type="text"  />
+			<input type="text"  placeholder="¿¿ Que tareas te falta ??" />
 			</form>
 			<ul>{listadetareas.map((elemento,indice)=>{
-				return (<li key={indice}>{elemento}<button onClick={()=>borra(indice)}>Borrar</button></li>)
+				return (<li key={indice}>{elemento}<button onClick={()=>borra(indice)}>x</button></li>)
 			})}
 
 			</ul>
